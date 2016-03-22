ea091670b69379ba767712729ef2a07e15f4f65f
Bailey Freund <freund.bailey@gmail.com>
2016-03-21 21:55:57 -0700

    Added java style guide comments

--- ChessModel.java
+++ ChessModel.java
@@ -10,7 +10,11 @@ public class ChessModel implements IChessModel {
 	private Player player;
 	public ArrayList<IChessPiece> takenWhite;
 	public ArrayList<IChessPiece> takenBlack;
-
+/**
+     *Constructor for ChessModel - instantiates all pieces and their
+     *respective locations on the board as well as the taken ArrayLists
+     *to empty.
+     */
 	public ChessModel() {
 		board = new IChessPiece[8][8];
 
@@ -46,7 +50,12 @@ public class ChessModel implements IChessModel {
 		takenWhite = new ArrayList<IChessPiece>();
 		takenBlack = new ArrayList<IChessPiece>();
 	}
-
+	/**
+     * Returns whether the game is complete based on whether the player
+     * is in CheckMate.
+     *
+     * @return {@code true} if complete, {@code false} otherwise.
+     */
 	public boolean isComplete() {
 		
 		if (inCheck(this.currentPlayer()) == true) {
@@ -91,13 +100,24 @@ public class ChessModel implements IChessModel {
 		return result;
 
 	}
-
+	/**
+     * Moves the piece from location {@code [move.fromRow, move.fromColumn]} to location {@code [move.fromRow,
+     * move.fromColumn]}.
+     *
+     * @param move a {@link W13project3.Move} object describing the move to be made.
+     * @throws IndexOutOfBoundsException if either {@code [move.fromRow, move.fromColumn]} or {@code [move.toRow,
+     *                                   move.toColumn]} don't represent valid locations on the board.
+     */
 	public void move(Move move) {
 		board[move.toRow][move.toColumn] = pieceAt(move.fromRow, move.fromColumn);
 		pieceAt(move.toRow, move.toColumn).setMoved(true);
 		board[move.fromRow][move.fromColumn] = null;
 	}
-
+	/**
+     * Report whether the current player p is in check.
+     * @param  p {@link W13project3.Move} the Player being checked
+     * @return {@code true} if the current player is in check, {@code false} otherwise.
+     */
 	public boolean inCheck(Player p) {
 		for (int x = 0; x < 8; x++) {
 			for (int y = 0; y < 8; y++) {
@@ -116,7 +136,16 @@ public class ChessModel implements IChessModel {
 
 		return false;
 	}
-
+	/**
+     * Creates an ArrayList of possible moves for every piece
+     * loops on every spot on the board to see whether the piece at r c
+     * will be able to move there
+     * 
+     * @param r The row of the piece being checked
+     * @param c The column of piece being checked
+     * @return results The ArrayList of possible moves of the piece in
+     * question
+     */
 	public ArrayList<Move> possibleMoves(int r, int c) {
 		ArrayList<Move> results = new ArrayList<Move>();
 		Move test;
@@ -132,23 +161,45 @@ public class ChessModel implements IChessModel {
 
 		return results;
 	}
-
+	/**
+     * Report who the current player is
+     * 
+     * @return player the current player
+     */
 	public Player currentPlayer() {
 		return player;
 	}
-
+	/**
+     * Report number of rows
+     * 
+     * @return 8 - num of rows is always 8
+     */
 	public int numRows() {
 		return 8;
 	}
-
+	/**
+     * Report number of columns
+     * 
+     * @return 8 - num of columns is always 8
+     */
 	public int numColumns() {
 		return 8;
 	}
-
+	/**
+     * Returns the piece at the selected row and column
+     * 
+     * @return the piece at the selected row and column on the 2d array
+     * of IChessPieces
+     */
 	public IChessPiece pieceAt(int row, int column) {
 		return board[row][column];
 	}
-
+	/**
+     * Returns Whether the selected spot contains a piece
+     * 
+     * @return true if there is a piece in the selected spot
+     * @return false if there is no piece in the seleceted spot
+     */
 	public boolean containsPiece(int row, int column) {// checks to see whether
 														// the row and column
 														// contains a piece -
@@ -166,12 +217,22 @@ public class ChessModel implements IChessModel {
 		}
 
 	}
-
+	/**
+     * Changes player
+     * 
+     * @return player that is next
+     */
 	public Player setNextPlayer() {
 		player = player.next();
 		return player;
 	}
-
+	/**
+     * Checks whether the king can castle on it's respective side
+     * Checks for both sides, long and short castling
+     * 
+     * @return true if castling is possible
+     * @return false if castling is not possible
+     */
 	public boolean canCastle(Move move) {
 		try{
 		if (move.fromRow == 7 && move.toColumn == 7) {
@@ -208,7 +269,13 @@ public class ChessModel implements IChessModel {
 
 		return false;
 	}
-
+	/**
+     * This method does the castling maneuver and returns whether
+     * it is possible to do the castle maneuver
+     * 
+     * @return true if you can castle
+     * @return false if you cannot castle
+     */
 	public boolean castle(Move move) {
 		// white long castling
 		if (move.fromRow == 7 && move.toColumn == 7) {
@@ -261,7 +328,12 @@ public class ChessModel implements IChessModel {
 
 		return false;
 	}
-
+	/**
+     * Adds taken pieces to an ArrayList of their respective color
+     * 
+     * @param piece The piece that has been taken and added to the array
+     * list
+     */
 	public void addToTaken(IChessPiece piece) {
 
 		if (piece.type() != null && Player.WHITE.equals(currentPlayer())) {
@@ -270,7 +342,12 @@ public class ChessModel implements IChessModel {
 			takenBlack.add(piece);
 		}
 	}
-
+	/**
+     * Gets the arrayList of the Taken pieces and returns it
+     * 
+     * @return takenWhite array of taken white pieces if player is white
+     * @return takenBlack array of taken white pieces if player is black
+     */
 	public ArrayList<IChessPiece> getTakenArrays() {
 		if (currentPlayer().equals(Player.WHITE)) {
 			return takenWhite;
@@ -278,7 +355,15 @@ public class ChessModel implements IChessModel {
 			return takenBlack;
 		}
 	}
-
+	/**
+     * Loops to find if there is a pawn in one of the "end zones"
+     * if a pawn is in one of the end zones, then returns true, if not
+     * returns false. Also calls the getTakenArrays before returning so
+     * that the panel can get the necessary data to display to the 
+     * user
+     * 
+     * @return player the current player
+     */
 	public boolean pawnAtEnd() {
 		int row = 0;
 		int col = 0;
@@ -301,7 +386,13 @@ public class ChessModel implements IChessModel {
 		}
 		return false;
 	}
-
+ 		/**
+	     * Swaps the pawn with the chosen chesspiece
+	     * 
+	     * @param piece piece chosen by user to swap
+	     * @param r row of piece being swapped
+	     * @param c col of piece being swapped
+	     */
 	public void pawnSwap(IChessPiece piece, int r, int c) {
 
 		board[r][c] = piece;
