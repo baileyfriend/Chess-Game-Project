058fd0542e88947d9aaba7bb08ea1337c6ca0fae
Bailey Freund <freund.bailey@gmail.com>
2016-03-21 22:09:13 -0700

    Java style guide

--- Knight.java
+++ Knight.java
@@ -2,18 +2,42 @@ package chess;
 
 public class Knight extends ChessPiece {
 
-
+	
+	/**
+     * Constructs piece and assigns a player
+     * 
+     * @param player player assigned to piece
+     */
 	public Knight(Player player){
 		super(player);
 		//super(isValidMove()); -- want to call the super's isValidMove to check for the conditions set in superClass ChessPiece - not exactly sure how to do that yet
 	}
-
+	/**
+     * Returns the type of the piece
+     * 
+     * @return Returns the type of the piece as a string
+     */
 	public String type(){
 		return "knight";
 	}
+	/**
+     *Sets whether the piece has been moved - only used by some pieces
+     * 
+     * @param val sets whether the piece has been moved
+     */
+	public void setMoved(boolean val) {
+		moved = val;
+	}//never used
 
 
-
+	/**
+	 * Returns whether the piece at location {@code [move.fromRow, move.fromColumn]} is allowed to move to location
+	 * {@code [move.fromRow, move.fromColumn]}.
+	 *
+	 * @param move  a {@link W13project3.Move} object describing the move to be made.
+	 * @param board the {@link W13project3.IChessBoard} in which this piece resides.
+	 * @return {@code true} if the proposed move is valid, {@code false} otherwise.
+	 */
 	public boolean isValidMove(Move move, IChessPiece[][] board){
 		
 		if(super.isValidMove(move, board)){
